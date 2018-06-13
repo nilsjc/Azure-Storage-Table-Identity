c70a6cac47f33b80f23c54c4e786fcef6b61ad2f
Nils Edvardsson <nilsedvardsson@outlook.com>
2018-06-13 12:54:24 +0200

    Added Interface and implementation for storage table access

---
+++ AzureStorageTableIdentity/AzureStorageTableIdentity/ISiteUserAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureStorageTableIdentity
+{
+    public interface ISiteUserAccess<T>
+    {
+        Task<T> FindByNameAsync(string normalizedUserName);
+    }
+}
---
+++ AzureStorageTableIdentity/AzureStorageTableIdentity/SiteUserAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureStorageTableIdentity
+{
+    public class SiteUserAccess<T> : ISiteUserAccess<T> where T : class, ITableEntity
+    {
+        private readonly CloudTable _table;
+        private readonly string _identityPartitionKey;
+        public SiteUserAccess(string connectionString, string tableName, string identityPartitionKey)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(tableName);
+            _identityPartitionKey = identityPartitionKey;
+            _table = table;
+        }
+
+        public Task<T> FindByNameAsync(string normalizedUserName)
+        {
+            var retrieve = TableOperation.Retrieve<T>(_identityPartitionKey, normalizedUserName);
+            var result = _table.ExecuteAsync(retrieve);
+            return Task.FromResult(result.Result.Result as T);
+        }
+    }
+}
